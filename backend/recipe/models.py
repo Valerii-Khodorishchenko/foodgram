3cd83b25164419c7fa093f0991451d7feddb35ff
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2025-01-09 03:03:59 +0500

    Добавлены модели рецептов и связанные с ней модели
    - Реализована модель Ingredient для хранения информации об ингредиентах
    (название и единица измерения).
    - Добавлена модель Tag для хранения тегов (название и идентификатор).
    - Создана основная модель Recipe, которая содержит:
        - Связь с ингредиентами через ManyToManyField.
        - Связь с тегами через ManyToManyField.
        - Поля для изображения, названия, описания и времени приготовления.
        - Поле автора рецепта (ForeignKey на модель пользователя).
    - Добавлена промежуточная модель RecipeComponent для указания количества
    ингредиентов в рецепте.
    - Настроены мета-данные для всех моделей: названия, множественные числа
    и порядок сортировки.
    - В моделях настроен вывод имени через __str__.

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -1,17 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from recipe.constants import (
     DESCRIPTION_LENGTH,
     FIRST_NAME_MAX_LENGTH,
+    INGR_NAME_MAX_LENGTH,
+    INGR_MUNIT_MAX_LENGTH,
+    RECIPE_NAME_MAX_LENGTH,
+    TAG_NAME_MAX_LENGTH,
+    TAG_SLUG_MAX_LENGTH,
     SECOND_NAME_MAX_LENGTH,
-    USERNAME_MAX_LENGTH
+    USERNAME_MAX_LENGTH,
+
 )
 from recipe.validators import validate_username
-
-
-class Recipe(models.Model):
-    pass
 
 
 class User(AbstractUser):
@@ -39,10 +42,10 @@ class User(AbstractUser):
         symmetrical=False
     )
     favorites = models.ManyToManyField(
-        Recipe, verbose_name='Избранные рецепты', related_name='favorites'
+        'Recipe', verbose_name='Избранные рецепты', related_name='favorites'
     )
     cart = models.ManyToManyField(
-        Recipe, verbose_name='Корзина', related_name='cart'
+        'Recipe', verbose_name='Корзина', related_name='cart'
     )
 
     USERNAME_FIELD = 'email'
@@ -55,3 +58,79 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username[:DESCRIPTION_LENGTH]
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Название ингредиента', max_length=INGR_NAME_MAX_LENGTH
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения', max_length=INGR_MUNIT_MAX_LENGTH
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name[:DESCRIPTION_LENGTH]
+
+
+class Tag(models.Model):
+    name = models.CharField('Название тега', max_length=TAG_NAME_MAX_LENGTH)
+    slug = models.SlugField('Идентификатор', max_length=TAG_SLUG_MAX_LENGTH)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ('slug',)
+
+    def __str__(self):
+        return self.name[:DESCRIPTION_LENGTH]
+
+
+class Recipe(models.Model):
+    ingredients = models.ManyToManyField(
+        Ingredient, verbose_name='Ингредиенты', related_name='recipes'
+    )
+    tags = models.ManyToManyField(
+        Tag, verbose_name='Теги', related_name='recipes'
+    )
+    image = models.ImageField('Изображение блюда', upload_to='dishes/')
+    name = models.CharField(
+        'Название рецепта',
+        max_length=RECIPE_NAME_MAX_LENGTH
+    )
+    text = models.TextField('Описание')
+    cooking_time = models.PositiveIntegerField(
+        verbose_name='Время приготовления',
+        help_text="Время приготовления в секундах",
+        validators=[MinValueValidator(1)]
+    )
+    author = models.ForeignKey(
+        User, verbose_name='Автор', on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name[:DESCRIPTION_LENGTH]
+
+
+class RecipeComponent(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField('Количество ингредиента')
+
+    class Meta:
+        verbose_name = 'Компонент блюда'
+        verbose_name_plural = 'Компоненты блюд'
+        ordering = ('recipe',)
+
+    def __str__(self):
+        return f"{self.ingredient.name[:DESCRIPTION_LENGTH]} ({self.quantity})"
