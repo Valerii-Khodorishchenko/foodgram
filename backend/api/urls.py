c6f417a72d4e85717e76dac6af7dab9d3d3956cd
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2025-03-12 22:53:12 +0500

    fix Удалена версия роутера

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,13 +10,13 @@ from api.views import (
 
 app_name = 'api'
 
-router_v1 = DefaultRouter()
-router_v1.register('users', UserViewSet, basename='users')
-router_v1.register('recipes', RecipeViewSet, basename='recipes')
-router_v1.register('tags', TagViewSet, basename='tags')
-router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
+router = DefaultRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
-    path('', include(router_v1.urls)),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
